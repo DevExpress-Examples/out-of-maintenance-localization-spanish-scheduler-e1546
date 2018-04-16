f50445bb21f62efa53cd0800adf6f5ac5f95103c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ VB/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace FrenchScheduler
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim timeRuler3 As New DevExpress.XtraScheduler.TimeRuler()
+			Dim timeRuler4 As New DevExpress.XtraScheduler.TimeRuler()
+			Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+			Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+			Me.dateNavigator1 = New DevExpress.XtraScheduler.DateNavigator()
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.dateNavigator1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' schedulerControl1
+			' 
+			Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.schedulerControl1.Location = New System.Drawing.Point(0, 44)
+			Me.schedulerControl1.Name = "schedulerControl1"
+			Me.schedulerControl1.Size = New System.Drawing.Size(580, 395)
+			Me.schedulerControl1.Start = New System.DateTime(2009, 6, 5, 0, 0, 0, 0)
+			Me.schedulerControl1.Storage = Me.schedulerStorage1
+			Me.schedulerControl1.TabIndex = 0
+			Me.schedulerControl1.Text = "schedulerControl1"
+			Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler3)
+			Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler4)
+			' 
+			' panelControl1
+			' 
+			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+			Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+			Me.panelControl1.Name = "panelControl1"
+			Me.panelControl1.Size = New System.Drawing.Size(759, 44)
+			Me.panelControl1.TabIndex = 2
+			' 
+			' dateNavigator1
+			' 
+			Me.dateNavigator1.Dock = System.Windows.Forms.DockStyle.Right
+			Me.dateNavigator1.Location = New System.Drawing.Point(580, 44)
+			Me.dateNavigator1.Name = "dateNavigator1"
+			Me.dateNavigator1.SchedulerControl = Me.schedulerControl1
+			Me.dateNavigator1.Size = New System.Drawing.Size(179, 395)
+			Me.dateNavigator1.TabIndex = 3
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(759, 439)
+			Me.Controls.Add(Me.schedulerControl1)
+			Me.Controls.Add(Me.dateNavigator1)
+			Me.Controls.Add(Me.panelControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.dateNavigator1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+		Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+		Private panelControl1 As DevExpress.XtraEditors.PanelControl
+		Private dateNavigator1 As DevExpress.XtraScheduler.DateNavigator
+	End Class
+End Namespace
+
